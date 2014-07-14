82986fa31c19b5672a13dd1ce40792be3c971f1a
Edward Lee <theelee13@gmail.com>
2014-07-14 16:20:43 -0400

    created a linked list used a tail-reference

---
+++ linkedList.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main(){
+	second := Prime{1,nil}
+	first := Prime{0,&second}
+	last := &second
+	for i:=2;i<10;i++{
+		newLast := Prime{i,nil}
+		last.next = &newLast
+		last = &newLast
+	}
+	for member := first;member.next!=nil;member=*member.next{
+		fmt.Println(member.val)
+	}
+}
+
+type Prime struct{
+	val int
+	next *Prime
+}
